b377504c5be2cd40921e258d42c775f4e78c313d
Jane <61692204+JaneMansell@users.noreply.github.com>
2023-03-10 14:56:15 +0000

    Add mouse tracking functions

--- javascript.js
+++ javascript.js
@@ -6,9 +6,34 @@ const gridContainer = document.querySelector('.gridContainer');
 gridContainer.style.gridTemplateColumns = `repeat(${gridWidth}, auto)`;
 gridContainer.style.gridTemplateRows = `repeat(${gridLength}, auto)`;
 
+
+
 for (let i = 0; i < gridSize; i++) {
     const gridBox = document.createElement('div');
     gridBox.classList.add('gridBox');
+    gridBox.setAttribute('id', 'grid' + i)
     gridContainer.appendChild(gridBox);
 }
 
+function colourBlack(e) {
+    console.log(e.toElement.id);
+    const square = document.querySelector(`#${e.toElement.id}`);
+    console.log(square)
+    square.classList.add('sketch');
+    window.addEventListener('mouseover', addColourClass);
+    window.addEventListener('mouseup', removeColourClass);
+
+}
+
+function addColourClass(e) {
+    console.log(e);
+}
+
+function removeColourClass(e) {
+    console.log(e);
+    window.removeEventListener('mouseover', addColourClass);
+}
+
+
+window.addEventListener('mousedown', colourBlack);
+window.addEventListener('mouseup', addColourClass);
