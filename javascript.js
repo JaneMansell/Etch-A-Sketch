b5f048ecec30e9c288575b5c56ec8e8179013997
Jane <61692204+JaneMansell@users.noreply.github.com>
2023-03-11 15:14:24 +0000

    Add change pixel size button

--- javascript.js
+++ javascript.js
@@ -3,8 +3,8 @@ function createGrid(gridWidth,gridLength) {
     const gridSize = gridWidth * gridLength;
 
     const gridContainer = document.querySelector('.gridContainer');
-    gridContainer.style.gridTemplateColumns = `repeat(${gridWidth}, auto)`;
-    gridContainer.style.gridTemplateRows = `repeat(${gridLength}, auto)`;
+    gridContainer.style.gridTemplateColumns = `repeat(${gridWidth}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${gridLength}, 1fr)`;
     
     for (let i = 0; i < gridSize; i++) {
         const gridBox = document.createElement('div');
@@ -41,9 +41,48 @@ function blankGrid() {
 
 }
 
+function removeExistingGrid() {
+    const gridContainer = document.querySelector('.gridContainer');
+    const gridBoxes = document.querySelectorAll('.gridBox');
+    gridBoxes.forEach((gridBox) => {
+        gridContainer.removeChild(gridBox);
+    });
+
+}
+
+function changePixels() {
+    let gridWidth = 0;
+    let gridLength = 0;
+    let i = 0;
+    while ((gridWidth < 1 || gridWidth > 64) && i<3) {
+        gridWidth = parseInt(prompt("How many pixels wide? Must be between 1 and 64"));
+        i++;
+    }
+    if (gridWidth < 1 || gridWidth > 64) {
+        alert("Input outside acceptable range");
+    }
+    
+    else{
+        i=0;
+        while ((gridLength < 1 || gridLength > 64) && i<3) {
+            gridLength = parseInt(prompt("How many pixels long? Must be between 1 and 64"));
+            i++;
+        }
+        if (gridLength < 1 || gridLength > 64) {
+            alert("Input outside acceptable range");
+        }
+        else {
+            removeExistingGrid();
+            createGrid(gridWidth, gridLength);
+        }
+    }
+}
+
 createGrid(32,16);
 
 window.addEventListener('mousedown', colourBlack);
 window.addEventListener('mouseup', addColourClass);
-const clearGrid = document.querySelector('#clearGrid');
-clearGrid.addEventListener('click', blankGrid);
+const clearGridButton = document.querySelector('#clearGrid');
+clearGridButton.addEventListener('click', blankGrid);
+const pixelChangeButton = document.querySelector('#pixelChange');
+pixelChangeButton.addEventListener('click', changePixels);
