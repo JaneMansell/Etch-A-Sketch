0c9e0ada7b47ad72899cabb9c91141c03982a5fc
Jane <61692204+JaneMansell@users.noreply.github.com>
2023-03-11 11:35:47 +0000

    Add black when mouse moves over page

--- javascript.js
+++ javascript.js
@@ -1,25 +1,21 @@
-const gridWidth = 16;
-const gridLength = 16;
-const gridSize = gridWidth * gridLength;
+function createGrid(gridWidth,gridLength) {
 
-const gridContainer = document.querySelector('.gridContainer');
-gridContainer.style.gridTemplateColumns = `repeat(${gridWidth}, auto)`;
-gridContainer.style.gridTemplateRows = `repeat(${gridLength}, auto)`;
+    const gridSize = gridWidth * gridLength;
 
-
-
-for (let i = 0; i < gridSize; i++) {
-    const gridBox = document.createElement('div');
-    gridBox.classList.add('gridBox');
-    gridBox.setAttribute('id', 'grid' + i)
-    gridContainer.appendChild(gridBox);
+    const gridContainer = document.querySelector('.gridContainer');
+    gridContainer.style.gridTemplateColumns = `repeat(${gridWidth}, auto)`;
+    gridContainer.style.gridTemplateRows = `repeat(${gridLength}, auto)`;
+    
+    for (let i = 0; i < gridSize; i++) {
+        const gridBox = document.createElement('div');
+        gridBox.classList.add('gridBox');
+        gridBox.setAttribute('id', 'grid' + i)
+        gridContainer.appendChild(gridBox);
+    }
 }
 
 function colourBlack(e) {
-    console.log(e.toElement.id);
-    const square = document.querySelector(`#${e.toElement.id}`);
-    console.log(square)
-    square.classList.add('sketch');
+    addColourClass;
     window.addEventListener('mouseover', addColourClass);
     window.addEventListener('mouseup', removeColourClass);
 
@@ -27,6 +23,9 @@ function colourBlack(e) {
 
 function addColourClass(e) {
     console.log(e);
+    const square = document.querySelector(`#${e.toElement.id}`);
+    console.log(square)
+    square.classList.add('sketch');
 }
 
 function removeColourClass(e) {
@@ -34,6 +33,7 @@ function removeColourClass(e) {
     window.removeEventListener('mouseover', addColourClass);
 }
 
+createGrid(32,16);
 
 window.addEventListener('mousedown', colourBlack);
 window.addEventListener('mouseup', addColourClass);
